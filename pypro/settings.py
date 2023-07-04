ce0e71bae320addb8503eba343bac18f349cbd65
matheuspdf <matheuslopes.pdf@gmail.com>
2023-07-03 22:33:08 -0300

    Desacopladas as configurações de instância da aplicação usando python-decouple.

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ni-oy7v_ndi_h(qj4sb1pg50ndji5zgut=ph@hfwx2%8gdot7v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = ['*']
 
