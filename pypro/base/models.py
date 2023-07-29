2df6f50525d8ec63737853524366a290cd4b015e
matheuspdf <matheuslopes.pdf@gmail.com>
2023-07-29 13:26:52 -0300

    correções

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from django.apps import apps
+# from django.apps import apps
 
 
 class UserManager(BaseUserManager):
@@ -22,10 +22,9 @@ class UserManager(BaseUserManager):
         # Lookup the real model class from the global app registry so this
         # manager method can be used in migrations. This is fine because
         # managers are by definition working on the real model.
-        GlobalUserModel = apps.get_model(
-            self.model._meta.app_label, self.model._meta.object_name
-        )
-        username = GlobalUserModel.normalize_username(email)
+        # GlobalUserModel = apps.get_model(
+        #     self.model._meta.app_label, self.model._meta.object_name
+        # )
         user = self.model(email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
