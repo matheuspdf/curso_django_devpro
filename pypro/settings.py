997f8593725b3ecc634f0dce853d6a4b22d1e2bc
Matheus Lopes <96511087+matheuspdf@users.noreply.github.com>
2023-08-01 20:57:03 -0300

    19 (#21)
    * usuário remoto configurado close #19
    
    * usuário remoto configurado close #19
    
    * usuário remoto configurado close #19

--- pypro/settings.py
+++ pypro/settings.py
@@ -168,3 +168,5 @@ if AWS_ACCESS_KEY_ID:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["https://curso-django-devpro.fly.dev"]
