7c7c374620cfb704c7580322532a65579397f18d
matheuspdf <matheuslopes.pdf@gmail.com>
2023-08-02 22:00:07 -0300

    debug toolbar configurado close #22

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -6,7 +6,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-# from django.apps import apps
 
 
 class UserManager(BaseUserManager):
