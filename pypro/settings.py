ed75222bc1eb97ebe8f1e504d65fa9df9673a3c5
matheuspdf <matheuslopes.pdf@gmail.com>
2023-08-04 18:28:42 -0300

    SDK configurado close #24

--- pypro/settings.py
+++ pypro/settings.py
@@ -14,6 +14,8 @@ from functools import partial
 from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -180,3 +182,18 @@ if AWS_ACCESS_KEY_ID:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["https://curso-django-devpro.fly.dev"]
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
