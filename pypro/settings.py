402f0028079839a204f59ef094b6e7ad67634554
matheuspdf <matheuslopes.pdf@gmail.com>
2023-08-08 22:28:23 -0300

    Início da landing page com Twitter Bootstrap e Layoutit

--- pypro/settings.py
+++ pypro/settings.py
@@ -16,6 +16,7 @@ from decouple import config, Csv
 import dj_database_url
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+5
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
