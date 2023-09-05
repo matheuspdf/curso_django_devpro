75b5c9e7bb2f481e291560c703c903910115a718
matheuspdf <matheuslopes.pdf@gmail.com>
2023-09-04 21:15:00 -0300

    Página criada para o segundo vídeo close #31

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'oo': {'titulo': 'Vídeo Aperitivo: OO em Python sem sotaque', 'vimeo_id': 856570782},
+        'iot': {'titulo': 'IOT com Luciano Ramalho', 'vimeo_id': 861071461}
+
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
