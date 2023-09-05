ae577866d1d81ca067da89d2f17db47a74ef4280
Matheus Lopes <96511087+matheuspdf@users.noreply.github.com>
2023-09-04 22:09:41 -0300

    Filtro de título adicionado (#34)
    close #33

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -5,7 +5,6 @@ def video(request, slug):
     videos = {
         'oo': {'titulo': 'Vídeo Aperitivo: OO em Python sem sotaque', 'vimeo_id': 856570782},
         'iot': {'titulo': 'IOT com Luciano Ramalho', 'vimeo_id': 861071461}
-
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
