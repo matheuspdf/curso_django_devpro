46b2dc3a876f3acca3c3c778716188df38ec4636
matheuspdf <matheuslopes.pdf@gmail.com>
2023-09-23 17:49:48 -0300

    Testes com banco de Dados + Django Admin

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,19 +1,12 @@
 from pypro.aperitivos.models import Video
-from django.shortcuts import render
-
-videos = [
-    Video(slug='oo', titulo='Vídeo Aperitivo: OO em Python sem sotaque', vimeo_id='856570782'),
-    Video(slug='iot', titulo='IOT com Luciano Ramalho', vimeo_id='861071461'),
-]
-
-
-videos_dct = {v.slug: v for v in videos}
+from django.shortcuts import render, get_object_or_404
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
