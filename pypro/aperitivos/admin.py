657bad31954446eb14eda927d1fc79945eff4a2c
matheuspdf <matheuslopes.pdf@gmail.com>
2023-09-28 21:02:15 -0300

    Testes com banco de Dados + Django Admin

--- pypro/aperitivos/admin.py
+++ pypro/aperitivos/admin.py
@@ -5,6 +5,6 @@ from pypro.aperitivos.models import Video
 
 @register(Video)
 class VideoAdmin(ModelAdmin):
-    list_display = ('titulo', 'slug', 'vimeo_id')
+    list_display = ('titulo', 'slug', 'creation', 'vimeo_id')
     ordering = ('creation',)
     prepopulated_fields = {'slug': ('titulo',)}
